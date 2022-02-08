1a3c3962468934c4ec33834a2f6390965d313f1d
Sumi Park <imuskrap@gmail.com>
2022-02-08 20:31:49 +0900

    todo js update

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,34 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)) 
+    // localStorage can only save strings
+    //js에서 localStorage.setItem()을 할 때, localStorage는 js의 오브젝트를 저장할 수 없다. object -> string 바꿔 localstorage에 저장해야한다.
+}
+
+function deleteToDO(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id
     const span = document.createElement("span");
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDO)
     li.appendChild(span);
-    span.innerText = newTodo;
-    toDoList.appendChild(li)
+    li.appendChild(button);
+    toDoList.appendChild(li);
 
 }
 
@@ -15,7 +37,33 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newTodo);
+    const newToDoObj = {
+        text: newTodo,
+        id : Date.now(),
+    };
+    toDos.push(newToDoObj); //array
+    paintToDo(newToDoObj);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit )
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if(savedToDos !== null){
+    const parsedToDos =  JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
+
+/*
+1. To-Do-List 작성시 localStorage 에 저장이 됩니다.
+2. 근데 저장이 될때 string data type 으로 저장이 되요. (예: "[a,b,c,d,e]")
+3. 그래서 JSON.parse()를 통해 string data type을 object로 바꾼거에요. 근데 이 Object는 Array 같이 바뀌었어요. 즉 index를 통해 value를 access할수 있어요
+예: "[a,b,c,d]" (string) => [a, b, c, d] (array);
+array[0] = a; array[1] = b; array[2] = c; array[3] = d
+5. array 형태가 된 값을 parsedToDos 라는 const variable 에다가 넣어놨어요.
+6. 이 상태에서 parsedToDos 는 array 형태라고 가정했을때 .foreach() 라는 function 을 사용할수 있는데 그냥 단순히 array 에 들어있는 모든 값을 iterate (순찰(?)) 할수 있는 function 입니다.
+7. 즉 index 0 부터 마지막 index 까지 한바뀌 도는건데 돌면서 그 값들을 item 라는 곳 또는 element에 (이름은 정하기 나름) 저장이 되는거에요.
+*/
